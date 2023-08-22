2f74f2055a94dcb1cd6f06cb2c4150667586028f
PAi <142898645+joony06@users.noreply.github.com>
2023-08-22 21:04:45 +0900

    edit graph

--- ECC/js/index.js
+++ ECC/js/index.js
@@ -1,17 +1,17 @@
 const BOARDID = 'box'; // Insert your id here!
 
-JXG.Options.axis.strokeColor = '#cccccc';
+JXG.Options.axis.strokeColor = 'black';
 const board = JXG.JSXGraph.initBoard(BOARDID, {
-    boundingbox: [-15, 10, 15, -10],
+    boundingbox: [-20, 20, 20, -20],
     axis: true
 });
 
-var b = board.create('slider', [[1, -3], [10, -3], [-10, 2.10, 10]], {name: 'a'});
-var a = board.create('slider', [[1, -4], [10, -4], [-10, -9.52, 10]], {name: 'b'});
+let a = board.create('slider', [[2, -5], [11, -5], [-20, 0, 20]], {name: 'a'});
+let b = board.create('slider', [[2, -7], [11, -7], [-20, 17, 20]], {name: 'b'});
 
-var c1 = board.create('functiongraph', [function(x) {
+let c1 = board.create('functiongraph', [function(x) {
     return Math.sqrt(x * x * x + a.Value() * x + b.Value());
 }], {strokeWidth: 3, strokeColor: 'black'});
-var c2 = board.create('functiongraph', [function(x) {
+let c2 = board.create('functiongraph', [function(x) {
     return -Math.sqrt(x * x * x + a.Value() * x + b.Value());
 }], {strokeWidth: 3, strokeColor: 'black'});
