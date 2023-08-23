93ed258012173dcfb768b215f766dbb24dea54bc
PAi <142898645+joony06@users.noreply.github.com>
2023-08-23 11:04:02 +0900

    Update app.js

---
+++ Main Page/assets/js/app.js
@@ -0,0 +1,29 @@
+function fetchPage(name) {
+    fetch(name).then(function(response) {
+        response.text().then(function(text){
+            document.querySelector('#two').innerHTML = text;
+        })
+    });
+}
+
+if(location.hash){
+    fetchPage(location.hash.substring(2));
+} else {
+    fetchPage('welcom');
+}
+
+fetchPage('list').then(function(response) {
+    response.text().then(function(text) {
+        let items = text.split(',');
+        let i = 0;
+        let tags = '';
+        while(i<items.length) {
+            let item = item[i];
+            item = item.trim();
+            let tag = `<li><a href="#!${item}" onclick="fetchPage('${item}')">${item}</a></li>`;
+            tags = tags = tag;
+            i = i + 1;
+        }
+        document.querySelector('#nav').innerHTML = tags;
+    })
+});
